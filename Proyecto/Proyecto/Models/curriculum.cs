6025de44182517adb7dff583762d642705360fc5
edwin fuentes <fedwin363@gmail.com>
2019-05-11 01:29:45 -0600

    Agregando login sin permisos

--- Proyecto/Proyecto/Models/curriculum.cs
+++ Proyecto/Proyecto/Models/curriculum.cs
@@ -7,7 +7,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespaceProyecto.Models
+namespace Proyecto.Models
 {
     using System;
     using System.Collections.Generic;
