c09afa6f26ce74788b437b25e848d461eb489d94
luisedu99 <luis.santos748@yahoo.com>
2019-05-15 00:01:52 -0600

    Archivo cambiado 3

--- Proyecto/Proyecto/Models/ofertasModel.cs
+++ Proyecto/Proyecto/Models/ofertasModel.cs
@@ -2,10 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Proyecto.Models;
 
 namespace Proyecto.Models
 {
-    public class ofertasModel: AbstractModel<ofertas>
+    public class ofertasModel : AbstractModel<ofertas>
     {
     }
 }
